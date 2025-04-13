a3f1d8d48f4ea91dfa9d6f2eaf66a63858321dc5
Guy Marshall <guymarshall9@gmail.com>
2025-04-13 22:04:22 +0100

    Initial commit

---
+++ app/src/main/java/com/marshall/guy/redscreen/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.marshall.guy.redscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.marshall.guy.redscreen.ui.theme.RedScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            RedScreenTheme {
+                RedBackground()
+            }
+        }
+    }
+}
+
+@Composable
+fun RedBackground() {
+    androidx.compose.foundation.layout.Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+    )
+}
